0fbae9fea04fa0276a3bd6095e8f697c10a584f0
karpyzin <karp123@inbox.ru>
2023-12-19 21:15:45 +0700

    init

--- shared/src/androidMain/kotlin/ru/cmtt/sdk/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package ru.cmtt.sdk
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/ru/cmtt/sdk/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package ru.cmtt.sdk
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
